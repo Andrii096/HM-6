a0d63634740b2ac6ce371b4f87df151181dad91f
Andrii096 <148378796+Andrii096@users.noreply.github.com>
2024-01-12 20:08:52 +0100

    clean.py

--- clean.py
+++ clean.py
@@ -46,7 +46,10 @@ registered_extensions = {
     "XLSX" : documents,
     "ZIP": archives,
     "MP3": audio,
-    "MP4": video
+    "MP4": video,
+    "AVI": video,
+    "MKV": video,
+    "MOV": video
 }
 
 
@@ -57,7 +60,7 @@ def get_extensions(file_name):
 def scan(folder):
     for item in folder.iterdir():
         if item.is_dir():
-            if item.name not in ("JPEG", "JPG", "PNG", "TXT", "DOCX", "DOC", "PDF", "PPTX", "XLSX" "OTHER", "ZIP", "MP3", "MP4"):
+            if item.name not in ("JPEG", "JPG", "PNG", "TXT", "DOCX", "DOC", "PDF", "PPTX", "XLSX" "OTHER", "ZIP", "MP3", "MP4","AVI","MKV","MOV"):
                 folders.append(item)
                 scan(item)
             continue
@@ -148,3 +151,12 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    print(f"Images: {images}\n")
+    print(f"Video: {video}\n")
+    print(f"Audio: {audio}\n")
+    print(f"Documents: {documents}\n")
+    print(f"Archive: {archives}\n")
+    print(f"Unknown: {others}\n")
+    print(f"All extensions: {extensions}\n")
+    print(f"Unknown extensions: {unknown}\n")
